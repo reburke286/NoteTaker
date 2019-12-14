90db6c72344ca8f3a66ac1d2fc9c488384dbc57b
Becca Burke <reburke286@gmail.com>
2019-12-13 21:39:32 -0800

    final push before heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require("fs-extra");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 7000;
+const PORT = process.env.PORT || 7000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
